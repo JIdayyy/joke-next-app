17cd3376481cb7428b5f6495885acae6832fd481
JIdayyy <julien_abbadie@hotmail.fr>
2022-10-05 07:33:47 +0200

    NextSeo component in Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { Button, Center, Flex, Text, useColorMode } from "@chakra-ui/react";
 import JokeCard, { getJoke } from "@components/JokeCard";
 import { useQuery } from "react-query";
-import { NextSeo } from "next-seo";
 import MainLayout from "@components/Layouts/MainLayout";
 import CreateJokeModal from "@components/Modal/CreateJokeModal";
 
@@ -41,40 +40,6 @@ function Home(): JSX.Element {
             minH="100vh"
             minW="full"
         >
-            <NextSeo
-                title="Next joke API"
-                description="This example of Next.js with React Query and Chakra UI"
-                canonical="https://joke-next-app.vercel.app/"
-                openGraph={{
-                    url: "https://joke-next-app.vercel.app/",
-                    title: "Next Joke App",
-                    description: "Super Joke app",
-                    images: [
-                        {
-                            url: "https://www.example.ie/og-image-01.jpg",
-                            width: 800,
-                            height: 600,
-                            alt: "Og Image Alt",
-                            type: "image/jpeg",
-                        },
-                        {
-                            url: "https://www.example.ie/og-image-02.jpg",
-                            width: 900,
-                            height: 800,
-                            alt: "Og Image Alt Second",
-                            type: "image/jpeg",
-                        },
-                        { url: "https://www.example.ie/og-image-03.jpg" },
-                        { url: "https://www.example.ie/og-image-04.jpg" },
-                    ],
-                    site_name: "Next Joke App",
-                }}
-                twitter={{
-                    handle: "@handle",
-                    site: "@site",
-                    cardType: "summary_large_image",
-                }}
-            />
             <Text fontSize={[20, 25, 30]} fontWeight="bold">
                 Wanna have fun for a minute ? 😁
             </Text>
